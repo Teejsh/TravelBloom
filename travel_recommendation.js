ae636da1b6f466821e8304dae5b1d88e993840eb
Teejsh <insanebreed@gmail.com>
2024-08-07 13:08:57 -0400

    more js functionality to fetch data from the json file

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,11 @@
 function searchCondition() {
-        const input = document.getElementById('seaInput').value.toLowerCase();
-        const resultDiv = document.getElementById('result');
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('travel_recommendation.json')
+        .then(response => response.json())
+        .then(data => {
+            const search = data.countries.find(item => item.name.toLowerCase() === input);
+        })
 }
